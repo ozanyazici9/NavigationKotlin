9e825cdf9ab6c4d4113772b676bb5f9e7fba694f
Ozan YAZICI <ozanyazici9@gmail.com>
2023-12-08 17:44:05 +0300

    Initial commit

---
+++ app/src/main/java/com/ozanyazici/navigationkotlin/FirstFragment.kt
@@ -0,0 +1,40 @@
+package com.ozanyazici.navigationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ozanyazici.navigationkotlin.databinding.FragmentFirstBinding
+
+
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentFirstBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.button1.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment("ozan yazıcı")
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
---
+++ app/src/main/java/com/ozanyazici/navigationkotlin/SecondFragment.kt
@@ -0,0 +1,45 @@
+package com.ozanyazici.navigationkotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.ozanyazici.navigationkotlin.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+
+    private lateinit var binding: FragmentSecondBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            val kullaniciAdi = SecondFragmentArgs.fromBundle(it).username
+            binding.textView2.text= kullaniciAdi
+        }
+
+        binding.button2.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+}
